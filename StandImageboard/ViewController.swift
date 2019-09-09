3afe11fa980b39690368b4e39742ac6d54b5e664
Adan Partida <ada2029617@maricopa.edu>
2019-09-09 11:19:21 -0700

    Added buttons, images and actions

--- StandImageboard/ViewController.swift
+++ StandImageboard/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var StandImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func StarPlatinumButton(_ sender: Any) {
+        StandImage.image = UIImage(named: "star_platinum")
+    }
+    
+    @IBAction func TheWorldButton(_ sender: Any) {
+        StandImage.image = UIImage(named: "the_world")
+    }
+    
+    
+    @IBAction func HighwayStarbutton(_ sender: Any) {
+        StandImage.image = UIImage(named: "highway_star")
+    }
+    
+    @IBAction func HierophantGreenButton(_ sender: Any) {
+        StandImage.image = UIImage(named: "hierophant_green")
+    }
+    
+    @IBAction func KillerQueenButton(_ sender: Any) {
+        StandImage.image = UIImage(named: "killer_queen")
+    }
+    
+    @IBAction func KingCrimsonButton(_ sender: Any) {
+        StandImage.image = UIImage(named: "king_crimson")
+    }
+    
+    @IBAction func DivineAnubisButton(_ sender: Any) {
+        StandImage.image = UIImage(named: "divine_anubis")
+        
+        
+        
+        
+    }
 }
 
